01e9efe68f29a143e650fa98a0d529b05a23fc97
avo1 <voanhdung@gmail.com>
2016-11-04 21:56:11 +0700

    initial commit

---
+++ EmbededVC/EmbededViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EmbededViewController.swift
+//  EmbededVC
+//
+//  Created by Dave Vo on 11/4/16.
+//  Copyright © 2016 DaveVo. All rights reserved.
+//
+
+import UIKit
+
+protocol ContainerToMaster {
+    func changeLabel(text: String)
+}
+
+class EmbededViewController: UIViewController {
+
+    @IBOutlet weak var myTextField: UITextField!
+    var myText = ""
+    
+    var containerToMaster: ContainerToMaster?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onUpdate(_ sender: AnyObject) {
+        containerToMaster?.changeLabel(text: myTextField.text!)
+    }
+
+    func changeEmbededLabel(text: String) {
+        myTextField.text = text
+    }
+}
--- EmbededVC/ViewController.swift
+++ EmbededVC/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var embededView: UIView!
+    
+    @IBOutlet weak var textFieldA: UITextField!
+    
+    var containerVC: EmbededViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onTab(_ sender: UIButton) {
+        containerVC.changeEmbededLabel(text: textFieldA.text!)
     }
 
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "mySegue" {
+            containerVC = segue.destination as? EmbededViewController
+            containerVC!.containerToMaster = self
+        }
+    }
+}
+
+extension ViewController: ContainerToMaster {
+    func changeLabel(text: String) {
+        textFieldA.text = text
+    }
 }
 
